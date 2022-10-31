f24557e90ff6ad86fd3f0af20d2e57f97a64c6a4
Nekeey <nikitashrestha@gmail.com>
2022-10-31 15:22:10 +0545

    first commit

---
+++ Webb/Views/Test/TestController.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Webb.Views.Test
+{
+    public class TestControllerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
